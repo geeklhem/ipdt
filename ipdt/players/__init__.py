2084d923b548e38f67971814d45a22d431a83a81
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-11 19:22:22 +0200

    Catch the exception on the loading of a strategy

--- ipdt/players/__init__.py
+++ ipdt/players/__init__.py
@@ -1,13 +1,21 @@
 """In this modules, each player is stored as a submodule with a
-"Player" class inheriting from ipdt.player.Player"""
+"Player" class inheriting from ipdt.player.Player.
+
+This __init__ files load all the submodules in the ipdt.players
+namespace.
+"""
 import pkgutil
 import logging
 logger = logging.getLogger("ipdt")
 
 __all__ = []
 
-for loader, module_name, is_pkg in  pkgutil.iter_modules(__path__):  
-    __all__.append(module_name)
-    __import__("ipdt.players."+module_name)
+for loader, module_name, is_pkg in  pkgutil.iter_modules(__path__):
+    try:
+        __import__("ipdt.players."+module_name)
+    except Exception as e:
+        logger.critical("Strategy {} thrown an exception on import: {}.".format(module_name,e))
+    else:
+        __all__.append(module_name)
 
 logger.debug("loaded {} players: {}".format(len(__all__),__all__))
