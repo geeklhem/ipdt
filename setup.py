d8136022106b6c8dbe58d245c5d0201293c3f8d2
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-11 09:58:28 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ipdt',
+      version='0.1',
+      description='Iterate Prisoner Dilemma Tournament',
+      url='http://github.com/geeklhem/ipdt',
+      author='Guilhem Doulcier',
+      author_email='guilhem.doulcier@ens.fr',
+      license='GPLV3',
+      packages=['ipdt'],
+      zip_safe=False)
