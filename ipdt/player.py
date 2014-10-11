11368d0fb8312c97514d6fdf7ec8ece03a6cb59e
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-11 18:45:24 +0200

    added author in the strategies

--- ipdt/player.py
+++ ipdt/player.py
@@ -3,6 +3,7 @@
 class Player(object):
     """ The base class for every player """
     name = "Generic player"
+    author = "GT-mathbio"
     def __init__(self, param):
         self.param = param
     def play(self, last_coup):
