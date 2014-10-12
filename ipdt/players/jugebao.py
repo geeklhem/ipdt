42f05bb80212a3268143dd3ba0c818a65971b35f
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-12 15:37:17 +0200

    Le Juge Bao - Une stratégie par François.

---
+++ ipdt/players/jugebao.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import ipdt.player
+from random import random
+
+class Player(ipdt.player.Player):
+
+    """Il est dur, mais juste. Rationnel, mais imprévisible.
+    Le juge Bao a décidé de coopérer avec ceux qui veulent coopérer mais de ne
+    pas se laisser emmerder par les autres."""
+
+    name = "Le juge Bao"
+    author = "François Bienvenu"
+
+    def __init__(self, param):
+        self.p = 0.5
+        self.n = 2
+
+    def play(self, last_move):
+        if last_move == None :
+            return True
+        elif last_move == True :
+            self.p = (self.p * self.n + 1) / (self.n + 1)
+        else :
+            self.p = self.p * self.n / (self.n + 1)
+        self.n += 1
+        return random() < self.p
