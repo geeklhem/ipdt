162a9a119566fef272a2d1f9d2c1738cfd6ef235
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2014-10-13 15:09:38 +0200

    retaliator modified

--- ipdt/players/retaliator.py
+++ ipdt/players/retaliator.py
@@ -1,14 +1,13 @@
 import ipdt.player
 
 class Player(ipdt.player.Player):
-    """Retailator, a strategy that never forgets."""
-    name = "Retailator"
+    """Retaliator, a strategy that never forgets."""
+    name = "Retaliator"
     def __init__(self,param):
         self.revenge = False
     def play(self,last_move):
         if last_move is not None and not last_move:
             self.revenge = True
-
         if self.revenge:
             return False
         else:
